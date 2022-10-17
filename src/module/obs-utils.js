55bd8847d96d0bf57522306f66a55f7d399d0d93
Faey Umbrea <1144986+FaeyUmbrea@users.noreply.github.com>
2022-10-17 19:42:22 +0200

    Linting and Git Hooks

--- src/module/obs-utils.js
+++ src/module/obs-utils.js
@@ -1,130 +1,135 @@
-import {isOBS} from './utils/obs.mjs';
-import { hideApplication,hideTokenBorder,tokenMoved,startCombat,passTurn,stopCombat,getCurrentUser,viewportChanged,mode, isGM, expandTokenHud, scaleToFit } from './utils/helpers.mjs';
+/* global socketlib */
+
+import { isOBS } from './utils/obs.mjs';
+import {
+  hideApplication,
+  hideTokenBorder,
+  tokenMoved,
+  startCombat,
+  passTurn,
+  stopCombat,
+  getCurrentUser,
+  viewportChanged,
+  mode,
+  isGM,
+  expandTokenHud,
+  scaleToFit,
+} from './utils/helpers.mjs';
 import { generateDataBlockFromSetting, getSetting, registerSettings, setSetting } from './utils/settings.mjs';
 import Director from './applications/director.mjs';
 import '../less/obs-utils.less';
 
-const ID = "obs-utils";
+const ID = 'obs-utils';
 
-let socket;
+let modulesocket;
 
-function changeViewport(viewport, userId){
-	if(isOBS()) viewportChanged(viewport, userId)
-};
-
-async function changeMode(){
-	//Using an object to avoid reading Settings every time a token moves
-	mode.normal = await getSetting("defaultOutOfCombat");
-	mode.combat = await getSetting("defaultInCombat");
-	scaleToFit();
+function changeViewport(viewport, userId) {
+  if (isOBS()) viewportChanged(viewport, userId);
 }
 
-function changeCloneTarget(target){
-	if(isOBS()){
-		mode.trackedPlayer = target;
-	}
+async function changeMode() {
+  //Using an object to avoid reading Settings every time a token moves
+  mode.normal = await getSetting('defaultOutOfCombat');
+  mode.combat = await getSetting('defaultInCombat');
+  scaleToFit();
 }
 
-Hooks.once("socketlib.ready", () => {
-    socket = socketlib.registerModule(ID);
+function changeCloneTarget(target) {
+  if (isOBS()) {
+    mode.trackedPlayer = target;
+  }
+}
 
-    socket.register("viewport", changeViewport);	
-    socket.register("modechange", changeMode);	
-	socket.register("cloneTarget", changeCloneTarget)
+Hooks.once('socketlib.ready', () => {
+  modulesocket = socketlib.registerModule(ID);
+
+  modulesocket.register('viewport', changeViewport);
+  modulesocket.register('modechange', changeMode);
+  modulesocket.register('cloneTarget', changeCloneTarget);
 });
 
-function socketCanvas(_canvas, position){
-    socket.executeForOthers("viewport", position, getCurrentUser());
+function socketCanvas(_canvas, position) {
+  modulesocket.executeForOthers('viewport', position, getCurrentUser());
 }
 
-function buildButtons(buttons){
-	
-	if (!game.user.isGM) return;
-    	var buttonGroup = buttons.find(element => element.name === "token");
-    	if (!buttonGroup) return;
-    	buttonGroup.tools.push({
-        icon: "fa-solid fa-signal-stream",
-        name: "openStreamDirector",
-        title: "Open Stream Director",
-        toggle: true,
-        onClick: openDirector
-    	})
+function buildButtons(buttons) {
+  if (!game.user.isGM) return;
+  var buttonGroup = buttons.find((element) => element.name === 'token');
+  if (!buttonGroup) return;
+  buttonGroup.tools.push({
+    icon: 'fa-solid fa-signal-stream',
+    name: 'openStreamDirector',
+    title: 'Open Stream Director',
+    toggle: true,
+    onClick: openDirector,
+  });
 
-	console.warn(buttons)
+  console.warn(buttons);
 }
 
-async function sendMode(isInCombat, mode){
-	if(isInCombat)await setSetting("defaultInCombat",mode);
-	else await setSetting("defaultOutOfCombat",mode);
-	socket.executeForOthers("modechange");
+async function sendMode(isInCombat, newmode) {
+  if (isInCombat) await setSetting('defaultInCombat', newmode);
+  else await setSetting('defaultOutOfCombat', newmode);
+  modulesocket.executeForOthers('modechange');
 }
 
-async function sendTrack(playerID){
-	if(game.user.isGM)
-	socket.executeForOthers("cloneTarget", playerID)
+async function sendTrack(playerID) {
+  if (game.user.isGM) modulesocket.executeForOthers('cloneTarget', playerID);
 }
 
-function openDirector(){
-	let d = new Director(generateDataBlockFromSetting(sendMode,sendTrack))
-	d.render(true);
+function openDirector() {
+  let d = new Director(generateDataBlockFromSetting(sendMode, sendTrack));
+  d.render(true);
 }
 
+function start() {
+  Hooks.once('init', async function () {
+    registerSettings();
+  });
 
-function start(){	
+  Hooks.once('ready', async function () {
+    if (isGM()) {
+      Hooks.on('renderTokenHUD', expandTokenHud);
+    }
+  });
 
-	Hooks.once('init', async function() {
-		registerSettings();
-	});
-	
+  Hooks.on('canvasPan', socketCanvas);
 
-	Hooks.once('ready', async function() {
-		if(isGM()){
-			Hooks.on("renderTokenHUD", expandTokenHud);
-		}
-	});
+  if (isOBS()) {
+    Hooks.once('ready', async function () {
+      if (game.view == 'stream') $('body.stream').css('background-color', 'transparent');
+      //Init Mode Object
+      changeMode();
+      //Simulate a user interaction to start video playback
+      document.dispatchEvent(new KeyboardEvent('keydown', { key: 'a' }));
+    });
 
-	
-	Hooks.on("canvasPan", socketCanvas);
+    Hooks.once('canvasReady', scaleToFit);
 
-	if(isOBS()){
+    Hooks.on('renderSidebar', hideApplication);
+    Hooks.on('renderSceneNavigation', hideApplication);
+    Hooks.on('renderMainMenu', hideApplication);
+    Hooks.on('renderSceneControls', hideApplication);
+    Hooks.on('renderTokenHUD', hideApplication);
+    Hooks.on('renderSidebarTab', hideApplication);
+    Hooks.on('renderUserConfig', hideApplication);
+    Hooks.on('renderCameraViews', hideApplication);
+    Hooks.on('renderPlayerList', hideApplication);
+    Hooks.on('renderHotbar', hideApplication);
 
-		Hooks.once("ready", async function() {
-			if(game.view == "stream") $('body.stream').css('background-color', 'transparent');
-			//Init Mode Object
-			changeMode();
-			//Simulate a user interaction to start video playback
-			document.dispatchEvent(new KeyboardEvent("keydown", {'key': 'a'}))
-		})
+    $('section#ui-left img#logo').remove();
 
-		Hooks.once("canvasReady",scaleToFit);
+    Hooks.on('drawToken', hideTokenBorder);
+    Hooks.on('refreshToken', hideTokenBorder);
 
-		Hooks.on("renderSidebar", hideApplication);
-		Hooks.on("renderSceneNavigation", hideApplication);
-		Hooks.on("renderMainMenu", hideApplication);
-		Hooks.on("renderSceneControls", hideApplication);
-		Hooks.on("renderTokenHUD", hideApplication);
-		Hooks.on("renderSidebarTab", hideApplication);
-		Hooks.on("renderUserConfig", hideApplication);
-		Hooks.on("renderCameraViews", hideApplication);
-		Hooks.on("renderPlayerList", hideApplication);
-		Hooks.on("renderHotbar", hideApplication);
+    Hooks.on('updateToken', tokenMoved);
 
-		$("section#ui-left img#logo").remove()
-
-		Hooks.on("drawToken", hideTokenBorder);
-		Hooks.on("refreshToken", hideTokenBorder);
-
-		Hooks.on("updateToken", tokenMoved);
-
-		Hooks.on("combatStart", startCombat);
-		Hooks.on("combatTurn", passTurn);
-		Hooks.on("combatEnd", stopCombat);
-
-
-	}
-	else{
-		Hooks.on("getSceneControlButtons", buildButtons);
-	}
+    Hooks.on('combatStart', startCombat);
+    Hooks.on('combatTurn', passTurn);
+    Hooks.on('combatEnd', stopCombat);
+  } else {
+    Hooks.on('getSceneControlButtons', buildButtons);
+  }
 }
 
 start();
--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,36 @@
 const config = {
-    root: "src/",
-    publicDir: 'public',
-    base: '/modules/obs-utils/',
-    resolve: {conditions: ['import', 'browser']},
-    esbuild: {
-        target: ['es2022', 'chrome100'],
-        keepNames: true   // Note: doesn't seem to work.
-     },
-    server: {
-      port: 30001,
-      open: true,
-      proxy: {
-        '^(?!/modules/obs-utils/)': 'http://localhost:30000/',
-        '/socket.io': {
-          target: 'ws://localhost:30000',
-          ws: true,
-        },
-      }
+  root: 'src/',
+  publicDir: 'public',
+  base: '/modules/obs-utils/',
+  resolve: { conditions: ['import', 'browser'] },
+  esbuild: {
+    target: ['es2022', 'chrome100'],
+    keepNames: true, // Note: doesn't seem to work.
+  },
+  server: {
+    port: 30001,
+    open: true,
+    proxy: {
+      '^(?!/modules/obs-utils/)': 'http://localhost:30000/',
+      '/socket.io': {
+        target: 'ws://localhost:30000',
+        ws: true,
+      },
     },
-    build: {
-      outDir: '../dist',
-      assetsDir: 'assets',
-      emptyOutDir: true,
-      sourcemap: true,
-      brotliSize: true,
-      lib: {
-        name: 'obs-utils',
-        entry: 'module/obs-utils.js',
-        formats: ['es'],
-        fileName: 'obs-utils'
-      }
+  },
+  build: {
+    outDir: '../dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: true,
+    brotliSize: true,
+    lib: {
+      name: 'obs-utils',
+      entry: 'module/obs-utils.js',
+      formats: ['es'],
+      fileName: 'obs-utils',
     },
-}
+  },
+};
 
 export default config;
