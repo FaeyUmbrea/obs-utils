eca3daa30206c11ffb2195a8856977c8cd65939c
Faey Umbrea <1144986+FaeyUmbrea@users.noreply.github.com>
2022-10-18 21:46:27 +0200

    Fix regression: Chat disappearing from /stream

--- src/module/obs-utils.js
+++ src/module/obs-utils.js
@@ -50,15 +50,17 @@ function start() {
 
   Hooks.on('canvasPan', socketCanvas);
 
-  if (isOBS()) {
+  if (!isOBS()) {
     Hooks.once('ready', async function () {
-      if (game.view == 'stream') $('body.stream').css('background-color', 'transparent');
+      //if (game.view == 'stream') $('body.stream').css('background-color', 'transparent');
       //Init Mode Object
       changeMode();
       //Simulate a user interaction to start video playback
       document.dispatchEvent(new KeyboardEvent('keydown', { key: 'a' }));
     });
 
+    Hooks.once('init', async function (){
+      if(game.view != "game") return;
     Hooks.once('canvasReady', scaleToFit);
 
     Hooks.on('renderSidebar', hideApplication);
@@ -82,6 +84,7 @@ function start() {
     Hooks.on('combatStart', startCombat);
     Hooks.on('combatTurn', passTurn);
     Hooks.on('combatEnd', stopCombat);
+    });
   } else {
     Hooks.on('getSceneControlButtons', buildButtons);
   }
