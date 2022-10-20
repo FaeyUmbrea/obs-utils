b6bb591fc447281a7e0ab9ea9aa27f669cb5a00b
Faey Umbrea <1144986+FaeyUmbrea@users.noreply.github.com>
2022-10-20 03:12:09 +0200

    Forgot some

--- src/module/utils/socket.mjs
+++ src/module/utils/socket.mjs
@@ -1,9 +1,8 @@
 /* global socketlib */
 
 import { getCurrentUser, scaleToFit, tokenMoved, viewportChanged } from './canvas.mjs';
-import { ID, mode } from './const.mjs';
+import { ID } from './const.mjs';
 import { isOBS } from './obs.mjs';
-import { getSetting } from './settings.mjs';
 
 let modulesocket;
 
@@ -21,9 +20,6 @@ function changeViewport(viewport, userId) {
 export async function changeMode() {
   if (!isOBS()) return;
   //Using an object to avoid reading Settings every time a token moves
-  mode.normal = await getSetting('defaultOutOfCombat');
-  mode.combat = await getSetting('defaultInCombat');
-  mode.trackedPlayer = await getSetting('trackedUser');
   scaleToFit();
   tokenMoved();
 }
