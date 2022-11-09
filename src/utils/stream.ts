c75f438087574b19511e57580cf8a209bfcc47fb
Faey Umbrea <1144986+FaeyUmbrea@users.noreply.github.com>
2022-11-09 03:40:51 +0100

    Only lint staged files

--- src/utils/stream.ts
+++ src/utils/stream.ts
@@ -1,40 +1,36 @@
-
-export interface OverlayData{
-    get backgroundImage(): string;
-    get components(): Array<OverlayComponent>;
+export interface OverlayData {
+  get backgroundImage(): string;
+  get components(): Array<OverlayComponent>;
 }
 
-export class SimpleTextOverlayData implements OverlayData{
-    backgroundImage: string = "";
-    components: Array<OverlayComponent> = new Array<OverlayComponent>();
+export class SimpleTextOverlayData implements OverlayData {
+  backgroundImage = '';
+  components: Array<OverlayComponent> = new Array<OverlayComponent>();
 }
 
-export interface OverlayComponent{
-    getData(options: any): string;   
-    setData(newData: string): void;
+export interface OverlayComponent {
+  getData(options: any): string;
+  setData(newData: string): void;
 }
 
-export class PlayerCharacterDataComponent implements OverlayComponent{
-    private _data: string = "";
+export class PlayerCharacterDataComponent implements OverlayComponent {
+  private _data = '';
 
-    getData(options: Actor): string {
-        return (options[this._data as keyof Actor] as string);
-    }
-    setData(newData: string): void {
-        this._data = newData;
-    }
-
+  getData(options: Actor): string {
+    return options[this._data as keyof Actor] as string;
+  }
+  setData(newData: string): void {
+    this._data = newData;
+  }
 }
 
-export class PlainTextComponent implements OverlayComponent{
+export class PlainTextComponent implements OverlayComponent {
+  private _data = '';
 
-    private _data: string= "";
-
-    getData(options: any): string {
-        return this._data;
-    }
-    setData(newData: string): void {
-        this._data = newData;
-    }
-    
+  getData(options: any): string {
+    return this._data;
+  }
+  setData(newData: string): void {
+    this._data = newData;
+  }
 }
