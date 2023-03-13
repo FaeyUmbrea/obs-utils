86a6de73b3362411b396583d9bbd6f273ad563e2
Faey Umbrea <1144986+FaeyUmbrea@users.noreply.github.com>
2023-03-13 17:01:20 +0100

    Revert "Don't use BS Local"
    This reverts commit 521d8a76cc03f53150e2e7d8ee3198e2cdff37ea.

--- playwright.browserstack.config.js
+++ playwright.browserstack.config.js
@@ -39,6 +39,8 @@ const config = {
         trace: 'on-first-retry',
         viewport: {width:1280,height:720}
     },
+    globalSetup: './tests/global-setup.js',
+    globalTeardown: './tests/global-teardown.js',
 
     /* Configure projects for major browsers */
     projects: [
