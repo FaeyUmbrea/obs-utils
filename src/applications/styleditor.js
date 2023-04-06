26e585c4765f7061c804889965103758c4bbf878
Faey Umbrea <1144986+FaeyUmbrea@users.noreply.github.com>
2023-04-07 01:46:45 +0200

    Converting the last application to TJS

--- src/applications/styleditor.js
+++ src/applications/styleditor.js
@@ -1,6 +1,7 @@
-const DICECTOR_TEMPLATE = 'modules/obs-utils/templates/formapps.hbs';
+import {SvelteApplication} from "@typhonjs-fvtt/runtime/svelte/application";
+import StyleEditorUi from "../svelte/StyleEditorUi.svelte";
 
-export default class StyleEditor extends FormApplication{
+export default class StyleEditor extends SvelteApplication {
   style;
   callback;
 
@@ -9,37 +10,25 @@ export default class StyleEditor extends FormApplication{
     this.style = style;
     this.callback = callback;
   }
-  async _updateObject(event, formData) {
-    if (!(formData instanceof Object)) throw new Error('Form Data Empty');
-    this.callback(formData['style']);
-  }
-
-  getData() {
-    return this.style;
-  }
 
   static get defaultOptions() {
-    return mergeObject(super.defaultOptions, {
+    return foundry.utils.mergeObject(super.defaultOptions, {
       classes: ['styleeditor'],
-      template: DICECTOR_TEMPLATE,
       id: 'styleeditor-application',
       title: 'Style Editor',
       //tabs: [{ navSelector: '.tabs', contentSelector: '.content', initial: 'onLoad' }],
       height: 125,
       width: 400,
       //resizable: true,
+      svelte: {
+        class: StyleEditorUi,
+        target: document.body
+      }
     });
   }
 
-  async _renderInner(data){
-    const html = await super._renderInner(data);
-    const StyleEditorUI = await (await import('../svelte/StyleEditor.svelte')).default;
-    new StyleEditorUI({
-      target: html.get(0),
-      props: {
-        style: this.style,
-      },
-    });
-    return html;
+  async close(options = {}) {
+    this.callback(this.style);
+    return super.close(options);
   }
 }
---
+++ src/svelte/StyleEditorUi.svelte
@@ -0,0 +1,25 @@
+<script>
+  import {ApplicationShell} from "@typhonjs-fvtt/runtime/svelte/component/core";
+  import {getContext} from "svelte";
+
+  export let elementRoot = void 0;
+  const items = ["a", "b"]
+
+  let application = getContext("#external").application
+
+  let style = application.style;
+
+  function close() {
+    application.style = style;
+    application.close();
+  }
+</script>
+
+<svelte:options accessors={true}/>
+
+<ApplicationShell bind:elementRoot>
+  <input bind:value={style} name="style" type="text"/>
+  <footer>
+    <button on:click={close}>Submit</button>
+  </footer>
+</ApplicationShell>
